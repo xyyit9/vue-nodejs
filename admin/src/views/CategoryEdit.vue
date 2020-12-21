4ecfb5bd518a423090d2e2491fc19fc5f6cbc3a7
alina <mixiu323@qq.com>
2020-12-21 21:55:06 +0800

    add second catrgory

--- admin/src/views/CategoryEdit.vue
+++ admin/src/views/CategoryEdit.vue
@@ -2,6 +2,16 @@
   <div class="about">
     <h1>{{ id ? "编辑" : "新建" }}分类</h1>
     <el-form label-width="100px" @submit.native.prevent="save">
+      <el-form-item label="上级分类">
+        <el-select v-model="model.parent">
+          <el-option
+            v-for="item in parents"
+            :key="item._id"
+            :label="item.name"
+            :value="item._id"
+          ></el-option>
+        </el-select>
+      </el-form-item>
       <el-form-item label="名称">
         <el-input v-model="model.name"></el-input>
       </el-form-item>
@@ -20,11 +30,17 @@ export default {
   data() {
     return {
       model: {},
+      parents: [],
     };
   },
   methods: {
     async save() {
-      const res = await this.$http.post("categories", this.model);
+      let res;
+      if (this.id) {
+        res = await this.$http.put(`categories/${this.id}`, this.model);
+      } else {
+        res = await this.$http.post("categories", this.model);
+      }
       console.log(res);
       this.$router.push("/categories/list");
       this.$message({
@@ -34,11 +50,15 @@ export default {
     },
     async fetch() {
       const res = await this.$http.get(`categories/${this.id}`);
-      console.log(res)
       this.model = res.data;
+    },
+    async fetchParents() {
+      const res = await this.$http.get(`categories`);
+      this.parents = res.data;
     },
   },
   created() {
+    this.fetchParents();
     this.id && this.fetch();
   },
 };
--- admin/src/views/CategoryList.vue
+++ admin/src/views/CategoryList.vue
@@ -3,6 +3,7 @@
     <h1>分类列表</h1>
     <el-table :data="items">
       <el-table-column prop="_id" label="ID" width="230"> </el-table-column>
+      <el-table-column prop="parent" label="上级分类"> </el-table-column>
       <el-table-column prop="name" label="分类名称"> </el-table-column>
       <el-table-column
       fixed="right"
@@ -10,6 +11,7 @@
       width="180">
       <template slot-scope="scope">
         <el-button type="text" size="small" @click="$router.push(`/categories/edit/${scope.row._id}`)">编辑</el-button>
+        <el-button type="text" size="small" @click="remove(scope.row)">删除</el-button>
       </template>
     </el-table-column>
     </el-table>
@@ -28,6 +30,20 @@ export default {
       const res = await this.$http.get("categories");
       this.items = res.data;
     },
+    async remove(row){
+       this.$confirm(`是否去欸的那个要删除分类"${row.name}"?`, '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(async () => {
+          const res=await this.$http.delete(`categories/${row._id}`)
+          res.success && this.$message({
+            type: 'success',
+            message: '删除成功!'
+          });
+          this.fetch();
+        })
+    }
   },
   created() {
     this.fetch();
