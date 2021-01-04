a34047b2ff907900ecd44e083df8625240374d9c
alina <mixiu323@qq.com>
2021-01-04 17:03:55 +0800

    P11

--- admin/src/views/CategoryList.vue
+++ admin/src/views/CategoryList.vue
@@ -3,14 +3,14 @@
     <h1>分类列表</h1>
     <el-table :data="items">
       <el-table-column prop="_id" label="ID" width="230"> </el-table-column>
-      <el-table-column prop="parent" label="上级分类"> </el-table-column>
+      <el-table-column prop="parent.name" label="上级分类"> </el-table-column>
       <el-table-column prop="name" label="分类名称"> </el-table-column>
       <el-table-column
       fixed="right"
       label="操作"
       width="180">
       <template slot-scope="scope">
-        <el-button type="text" size="small" @click="$router.push(`/categories/edit/${scope.row._id}`)">编辑</el-button>
+        <el-button type="text" size="small" @click="$router.push(`rest/categories/edit/${scope.row._id}`)">编辑</el-button>
         <el-button type="text" size="small" @click="remove(scope.row)">删除</el-button>
       </template>
     </el-table-column>
@@ -27,16 +27,16 @@ export default {
   },
   methods: {
     async fetch() {
-      const res = await this.$http.get("categories");
+      const res = await this.$http.get("rest/categories");
       this.items = res.data;
     },
     async remove(row){
-       this.$confirm(`是否去欸的那个要删除分类"${row.name}"?`, '提示', {
+       this.$confirm(`是否确定要删除分类"${row.name}"?`, '提示', {
           confirmButtonText: '确定',
           cancelButtonText: '取消',
           type: 'warning'
         }).then(async () => {
-          const res=await this.$http.delete(`categories/${row._id}`)
+          const res=await this.$http.delete(`rest/categories/${row._id}`)
           res.success && this.$message({
             type: 'success',
             message: '删除成功!'
