169b28f3f46674d2879c545702b3292b4639d024
alina <mixiu323@qq.com>
2021-03-11 16:38:56 +0800

    p16

--- admin/src/views/HeroEdit.vue
+++ admin/src/views/HeroEdit.vue
@@ -5,7 +5,10 @@
       <el-form-item label="名称">
         <el-input v-model="model.name"></el-input>
       </el-form-item>
-      <el-form-item label="图标">
+      <el-form-item label="称号">
+        <el-input v-model="model.title"></el-input>
+      </el-form-item>
+      <el-form-item label="头像">
         <!-- $http.defaults.baseURL是一个变量，表示当前的地址 -->
         <el-upload
           class="avatar-uploader"
@@ -17,12 +20,141 @@
           <i v-else class="el-icon-plus avatar-uploader-icon"></i>
         </el-upload>
       </el-form-item>
+      <el-form-item label="类型">
+        <el-select v-model="model.categories" multiple>
+          <el-option
+            v-for="item of categories"
+            :label="item.name"
+            :value="item._id"
+            :key="item._id"
+          ></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="难度">
+        <el-rate
+          style="margin-top: 0.6rem"
+          :max="9"
+          show-score
+          v-model="model.scores.difficult"
+        ></el-rate>
+      </el-form-item>
+      <el-form-item label="技能">
+        <el-rate
+          style="margin-top: 0.6rem"
+          :max="9"
+          show-score
+          v-model="model.scores.skills"
+        ></el-rate>
+      </el-form-item>
+      <el-form-item label="攻击">
+        <el-rate
+          style="margin-top: 0.6rem"
+          :max="9"
+          show-score
+          v-model="model.scores.attack"
+        ></el-rate>
+      </el-form-item>
+      <el-form-item label="生存">
+        <el-rate
+          style="margin-top: 0.6rem"
+          :max="9"
+          show-score
+          v-model="model.scores.survive"
+        ></el-rate>
+      </el-form-item>
+      <el-form-item label="顺风出装">
+        <el-select v-model="model.items1" multiple>
+          <el-option
+            v-for="item of items"
+            :label="item.name"
+            :value="item._id"
+            :key="item._id"
+          ></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="逆风出装">
+        <el-select v-model="model.items2" multiple>
+          <el-option
+            v-for="item of items"
+            :label="item.name"
+            :value="item._id"
+            :key="item._id"
+          ></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="使用技巧">
+        <el-input type="textarea" v-model="model.usageTips"></el-input>
+      </el-form-item>
+      <el-form-item label="对抗技巧">
+        <el-input type="textarea" v-model="model.battleTips"></el-input>
+      </el-form-item>
+      <el-form-item label="团战思路">
+        <el-input type="textarea" v-model="model.teamTips"></el-input>
+      </el-form-item>
       <el-form-item>
         <el-button type="primary" native-type="submit">保存</el-button>
       </el-form-item>
     </el-form>
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    id: {},
+  },
+  data() {
+    return {
+      items: [],
+      categories: [],
+      model: {
+        name: "",
+        avatar: "",
+        title: "",
+        scores: {},
+      },
+    };
+  },
+  methods: {
+    async save() {
+      if (this.id) {
+        await this.$http.put(`rest/heros/${this.id}`, this.model);
+      } else {
+        await this.$http.post("rest/heros", this.model);
+      }
+      this.$router.push("/heros/list");
+      this.$message({
+        type: "success",
+        message: "保存成功",
+      });
+    },
+    async fetch() {
+      const res = await this.$http.get(`rest/heros/${this.id}`);
+      // this.model = Object.assign({}, this.model, res.data);
+      this.model = { ...this.model, ...res.data };
+    },
+    async fetchCategories() {
+      const res = await this.$http.get(`rest/categories`);
+      this.categories = res.data;
+    },
+    async fetchItems() {
+      const res = await this.$http.get(`rest/items`);
+      this.items = res.data;
+    },
+    afterUpload(res) {
+      // 一开始在data里并不存在icon,所以vue可能存在赋值赋不上的情况
+      // 1. 可以使用this.$set强制显示声明
+      // 2. 可以在data中协商icon属性
+      this.model.avatar = res.url;
+    },
+  },
+  created() {
+    this.fetchCategories();
+    this.fetchItems();
+    this.id && this.fetch();
+  },
+};
+</script>
 
 <style>
 .avatar-uploader .el-upload {
@@ -49,47 +181,3 @@
   display: block;
 }
 </style>
-
-
-<script>
-export default {
-  props: {
-    id: {},
-  },
-  data() {
-    return {
-      model: {
-        name: '',
-        avatar: ''
-      },
-    };
-  },
-  methods: {
-    async save() {
-      if (this.id) {
-        await this.$http.put(`rest/heros/${this.id}`, this.model);
-      } else {
-        await this.$http.post("rest/heros", this.model);
-      }
-      this.$router.push("/heros/list");
-      this.$message({
-        type: "success",
-        message: "保存成功",
-      });
-    },
-    async fetch() {
-      const res = await this.$http.get(`rest/heros/${this.id}`);
-      this.model = res.data;
-    },
-    afterUpload(res) {
-      // 一开始在data里并不存在icon,所以vue可能存在赋值赋不上的情况
-      // 1. 可以使用this.$set强制显示声明
-      // 2. 可以在data中协商icon属性
-      this.model.avatar = res.url
-    },
-  },
-  created() {
-    this.id && this.fetch();
-  },
-};
-</script>
